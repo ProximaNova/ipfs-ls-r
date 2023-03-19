fbe20125973791afe9f321ab8bb2aff641a74a0b
ProximaNova <jaretrudd@gmail.com>
2023-03-18 18:48:36 -0600

    Update ipfs-ls-r.sh

--- ipfs-ls-r.sh
+++ ipfs-ls-r.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #usage: ./thisscript.sh $depthnumber $cid $cidtoexclude
-x20=" "
-echo "INFO - Run the ipfs daemon before running this script."
+echo "INFO - You may want to run the ipfs daemon before running this script."
 ipfs ls $2 > output1.txt
 
 if [ -z ${3+x} ]
@@ -9,8 +8,8 @@ then
     # var is unset
     echo -n ''
 else
-    # Exlude a folder via adding " " to the end of the line.
-    echo "INFO - Excluding $3"
+    # Exclude a folder via adding " " to the end of the line.
+    echo "INFO - /ipfs/$3 has been excluded."
     vim -c "%s/\($3.*\)/\1 /g | wq" output1.txt
 fi
 
@@ -26,6 +25,7 @@ then
     cids=$(grep "/$" output1.txt | wc -l)
     cid_ls=$(ipfs ls $cid1 | sed "s/\//\x5c\//g" | sed "s/^/ /g" | perl -pE "s/\n/\\\r/g")
     vim -c "%s/\($cid1.*\)/\1 \r$cid_ls/g | %s/\n\n/\r/g | wq" output1.txt
+    echo 'INFO - Level 2 done.'
     if [ $(grep "^ .*/$" output1.txt | wc -l) -gt 0 ]
     then
         cids=$(grep "/$" output1.txt | wc -l)
@@ -33,33 +33,45 @@ then
 	while [ $x -lt $cids ]
         do
             cid1=$(grep "^ \S.*/$" output1.txt | head -n 1 | sed "s/^ //g" | sed "s/ .*//g")
-            cid_ls=$(ipfs ls $cid1 | sed "s/\//\x5c\//g" | sed "s/^/ $x20/g" | perl -pE "s/\n/\\\r/g")
-            vim -c "%s/\($cid1.*\)/\1 \r$cid_ls/g | %s/\n\n/\r/g | wq" output1.txt
+            cid_ls=$(ipfs ls $cid1 | sed "s/\//\x5c\x5c\//g" | sed "s/^/| /g" | perl -pE "s/\n/\\\\\\r/g")
+            echo 'execute "%s/\\('$cid1'.*\\)/\\1 \\r'$cid_ls'/g"' > output2.txt
+            echo 'execute "%s/\\n\\n/\\r/g"' >> output2.txt
+            echo 'execute "wq"' >> output2.txt
+            vim "output1.txt" -c 'execute "so output2.txt"'
             x=$(( $x + 1 ))
         done
+        echo 'INFO - Level 3 done.'
     fi
-    if [ $(grep "^ $x20\S.*/$" output1.txt | wc -l) -gt 0 ]
+    if [ $(grep "^| \S.*/$" output1.txt | wc -l) -gt 0 ]
     then
         cids=$(grep "/$" output1.txt | wc -l)
         x=0
         while [ $x -lt $cids ]
         do
-            cid1=$(grep "^ $x20\S.*/$" output1.txt | head -n 1 | sed "s/^ $x20//g" | sed "s/ .*//g")
-            cid_ls=$(ipfs ls $cid1 | sed "s/\//\x5c\//g" | sed "s/^/ $x20$x20/g" | perl -pE "s/\n/\\\r/g")
-            vim -c "%s/\($cid1.*\)/\1 \r$cid_ls/g | %s/\n\n/\r/g | wq" output1.txt
+            cid1=$(grep "^| \S.*/$" output1.txt | head -n 1 | sed "s/^| //g" | sed "s/ .*//g")
+            cid_ls=$(ipfs ls $cid1 | sed "s/\//\x5c\x5c\//g" | sed "s/^/|| /g" | perl -pE "s/\n/\\\\\\r/g")
+            echo 'execute "%s/\\('$cid1'.*\\)/\\1 \\r'$cid_ls'/g"' > output2.txt
+            echo 'execute "%s/\\n\\n/\\r/g"' >> output2.txt
+            echo 'execute "wq"' >> output2.txt
+	    vim "output1.txt" -c 'execute "so output2.txt"'
             x=$(( $x + 1 ))
         done
+	echo 'INFO - Level 4 done.'
     fi
-    if [ $(grep "^ $x20$x20\S.*/$" output1.txt | wc -l) -gt 0 ]
+    if [ $(grep "^|| \S.*/$" output1.txt | wc -l) -gt 0 ]
     then
         cids=$(grep "/$" output1.txt | wc -l)
         x=0
         while [ $x -lt $cids ]
         do
-            cid1=$(grep "^ $x20$x20\S.*/$" output1.txt | head -n 1 | sed "s/^ $x20$x20//g" | sed "s/ .*//g")
-            cid_ls=$(ipfs ls $cid1 | sed "s/\//\x5c\//g" | sed "s/^/ $x20$x20$x20/g" | perl -pE "s/\n/\\\r/g")
-            vim -c "%s/\($cid1.*\)/\1 \r$cid_ls/g | %s/\n\n/\r/g | wq" output1.txt
+            cid1=$(grep "^|| \S.*/$" output1.txt | head -n 1 | sed "s/^|| //g" | sed "s/ .*//g")
+            cid_ls=$(ipfs ls $cid1 | sed "s/\//\x5c\x5c\//g" | sed "s/^/||| /g" | perl -pE "s/\n/\\\\\\r/g")
+            echo 'execute "%s/\\('$cid1'.*\\)/\\1 \\r'$cid_ls'/g"' > output2.txt
+            echo 'execute "%s/\\n\\n/\\r/g"' >> output2.txt
+            echo 'execute "wq"' >> output2.txt
+            vim "output1.txt" -c 'execute "so output2.txt"'
             x=$(( $x + 1 ))
         done
+        echo 'INFO - Level 5 done.'
     fi
 fi
